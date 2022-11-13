35d902180fd7c3a2004d9b3d2ea41dfcbd8985b5
polimoli <55150837+polimoli@users.noreply.github.com>
2022-11-13 12:56:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>Тестовое задание</title>
+   <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+   <h1>Авторизация</h1>
+   <form action="" method="">
+      <div>
+         <label>Логин</label>
+         <input type="text" name="login" placeholder="Введите ваш логин">
+      </div>
+      <div>
+         <label>Пароль</label>
+         <input type="password" name="pass" placeholder="Введите ваш пароль">
+      </div>
+      <button class="btn input">Вход</button>
+      <a href="registrashion.html" class="registration">Зарегистрироваться</a>
+   </form>
+</body>
+</html>
